e06cdcd9843e47a3aebfe5efa35271ec5539e781
Jeff Fessler <fessler@umich.edu>
2021-05-22 08:44:13 -0400

    raw

--- test/prompt.jl
+++ test/prompt.jl
@@ -1,7 +1,7 @@
 # prompt.jl
 
 using MIRTjim: prompt
-using Test: @test, @testset
+using Test: @test, @testset, @test_throws
 
 @testset "prompt" begin
     _tmp = prompt(:state) # save current state
@@ -9,4 +9,17 @@ using Test: @test, @testset
     @test prompt(:state) === :draw
     prompt()
     @test prompt(_tmp) isa Symbol # return to original state
+
+    io_in = IOBuffer("d")
+    prompt( ; io_in, io_out)
+    @test prompt(:state) == :draw
+    prompt(:prompt)
+
+    io_in = IOBuffer("n")
+    prompt( ; io_in, io_out)
+    @test prompt(:state) == :nodraw
+    prompt(:prompt)
+
+    io_in = IOBuffer("q")
+    @test_throws String prompt( ; io_in, io_out)
 end
