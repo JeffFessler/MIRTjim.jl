d2649988b8c18404360d4bf9f6e60e09ac6f0fa3
Jeff Fessler <fessler@umich.edu>
2021-06-22 12:40:11 -0400

    mv offset

--- test/jim.jl
+++ test/jim.jl
@@ -2,7 +2,6 @@
 
 using MIRTjim: jim
 using LaTeXStrings
-using OffsetArrays: OffsetArray
 using Test: @test, @test_throws
 
 include("isplot.jl")
@@ -21,7 +20,6 @@ jim(:clim)
 @isplot jim(rand(6,4), fft0=true)
 @isplot jim(x=1:4, y=5:9, rand(4,5), title="test4")
 @isplot jim(x=-9:9, y=9:-1:-9, (-9:9) * (abs.((9:-1:-9) .- 5) .< 3)', title="rev")
-@isplot jim(OffsetArray(rand(5,3), (-3,-2)), "offset")
 @isplot jim(ones(3,3)) # uniform
 jim(:abswarn, false)
 @isplot jim(complex(rand(4,3)))
