59ae6a8bee279825fb178a1e5a35e32c25e05ce2
Jeff Fessler <fessler@umich.edu>
2021-06-22 23:07:15 -0400

    fix docs

--- docs/lit/examples/1-examples.jl
+++ docs/lit/examples/1-examples.jl
@@ -9,47 +9,61 @@
 #md #     These examples are available as Jupyter notebooks.
 #md #     You can execute them online with [binder](https://mybinder.org/) or just view them with [nbviewer](https://nbviewer.jupyter.org/) by clicking on the badges above!
 
-# These examples show what `MIRTjim` is all about.
+# These examples illustrate how to use `MIRTjim`.
 
 # First we tell Julia we are using this package,
 
 using MIRTjim: jim
 
-# ## Simplest plot
+# ### Simple 2D image
 
 
-# Simplest example
+# The simplest example is a 2D array.
+# Note that `jim` is designed to show a function `f(x,y)`
+# sampled as an array `z[x,y]` so the 1st index is horizontal direction.
 
-z = ones(7) * (1:5)'
-jim(z)
+x, y = 1:9, 1:7
+f(x,y) = x * y^2
+z = f.(x, y') # 9 × 7 array
+jim(z ; xlabel="x", ylabel="y")
 
 
-# Compare with `Plots.heatmap` to see the difference:
+# Compare with `Plots.heatmap` to see the differences
+# (transpose, color, distractingly many ticks):
 
 import Plots
-Plots.heatmap(z')
+Plots.heatmap(z)
 
-# Images often should include a titles
-jim(z, "7 × 5 image")
+# Images often should include a title.
+jim(z, "hello")
 
 
-# ## OffsetArrays are handled naturally
+# ### OffsetArrays
+
+# `jim` displays the axes naturally.
 
 using OffsetArrays
-zo = OffsetArray(z, (-3,-2))
-jim(z)
+zo = OffsetArray(z, (-3,-1))
+jim(zo)
 
 
-# ## 3D arrays as a mosaic
-z3 = reshape(1:(6*5*4), (6, 5, 4))
+# ### 3D arrays
+
+# `jim` automatically makes 3D arrays into a mosaic.
+
+z3 = reshape(1:(9*7*6), (9, 7, 6))
 jim(z3)
 
 
-# ## Units are supported, with axis units appended
+# ### Units
+
+# `jim` supports units, with axis and colorbar units appended naturally,
+# thanks to UnitfulRecipes.jl
+
 using Unitful
 
-x = (1:7)u"m/s"
-y = (1:5)u"s"
+x = (1:9)u"m/s"
+y = (1:7)u"s"
 zu = x * y'
 jim(x, y, zu ;
     clim=(0,40).*u"m", xlabel="rate", ylabel="time", colorbar_title="distance")
@@ -57,6 +71,14 @@ jim(x, y, zu ;
 # See `UnitfulRecipes.jl` to customize the units
 
 
-# ## Options abound
+# ## Options
+
+# See the docstring for `jim` for the many options.  Here are some defaults.
 
 jim(:defs)
+
+# One can set "global" defaults using appropriate keywords from above list.
+
+jim(:colorbar, :none) # disable colorbar for subsequent figures
+jim(:yflip, false) # have "y" axis increase upward
+jim(rand(9,7))
