5324ef90253c3109086ff485b23998fe920586bb
Jeff Fessler <JeffFessler@users.noreply.github.com>
2021-11-14 08:19:04 -0500

    Refine yticks due to`yflip` for 3D  (#21)
    * docs
    
    * zyflip
    
    * tests

--- src/jim.jl
+++ src/jim.jl
@@ -30,6 +30,7 @@ const jim_table = Dict([
  :line3plot => true, # lines around sub image for 3d mosaic?
  :line3type => (:yellow),
  :ncol => 0,
+ :nrow => 0,
  :padval => nothing,
  :mosaic_npad => 1,
  :tickdigit => 1,
@@ -109,6 +110,8 @@ option
 - `gui` call `Plots.gui()` immediately?; default `false`
 - `prompt` call `prompt()` immediately?; default `false`
 - `ncol` for mosaicview for 3D and higher arrays; default `0` does auto select
+   "Number of tiles in column direction."
+- `nrow` for mosaicview for 3D and higher arrays; default `0` does auto select
 - `padval` padding value for mosaic view; default `minimum(z)`
 - `line3plot` lines around sub image for 3d mosaic; default `true`
 - `line3type` line type around sub image for 3d mosaic; default `(:yellow)`
@@ -233,6 +236,7 @@ function jim(z::AbstractArray{<:Number} ;
     line3plot = jim_def[:line3plot],
     line3type = jim_def[:line3type],
     ncol::Int = jim_def[:ncol],
+    nrow::Int = jim_def[:nrow],
     padval = nothing_else(jim_def[:padval], _mingood(z)),
     mosaic_npad::Int = jim_def[:mosaic_npad],
     fft0::Bool = jim_def[:fft0],
@@ -245,39 +249,70 @@ function jim(z::AbstractArray{<:Number} ;
     kwargs...
 )
 
+    # determine mosaic layout
+    n1,n2,n3 = size(z,1), size(z,2), prod(size(z)[3:end])
+    if ncol == 0 && nrow == 0
+        nrow = n3 * size(z,1)/size(z,2) # wider images means fewer columns
+        nrow *= 600/400 # typical plot aspect ratio
+        nrow = max(floor(Int, sqrt(nrow)), 1)
+        ncol = ceil(Int, n3 / nrow)
+    end
     if ncol == 0
-        ncol = floor(Int, sqrt(prod(size(z)[3:end])))
+        ncol = ceil(Int, n3 / nrow)
+    end
+    if nrow == 0
+        nrow = ceil(Int, n3 / ncol)
     end
 
-    if !yflip
+    if !yflip # handling yflip with 3D mosaic is tricky
         z = reverse(z, dims=2)
         yflip = !yflip
     end
 
-    n1,n2,n3 = size(z,1), size(z,2), size(z,3)
-    z = mosaicview(z ; fillvalue = padval, ncol, npad = mosaic_npad)
+    zz = mosaicview(z ; fillvalue = padval, ncol, nrow, npad = mosaic_npad)
     fft0 && @warn("fft0 option ignored for 3D")
 
-    xy = () # no x,y for mosaic
-    p = jim(z ; transpose = false, xy, xticks, yticks, yflip,
+    # adjusted x,y for mosaic
+    x = range(x[1]; length=size(zz,1), step=x[2]-x[1])
+    zyflip = false
+    if y[[1,end]] == axes(z,2)[[1,end]] # typical 1:ny case
+        y = range(y[1]; length=size(zz,2), step=y[2]-y[1])
+    else # ig.y setting
+        y = range(y[end]; length=size(zz,2), step=y[1]-y[2])
+        yflip = false
+        zz = reverse(zz, dims=2)
+        zyflip = true
+        y = reverse(y)
+    end
+    xy = (x,y)
+
+    p = jim(zz ; transpose = false, xy, xticks, yticks, yflip,
         gui=false, prompt=false, kwargs...)
 
     if n3 > 1 && line3plot # lines around each subimage
-
         n1 += mosaic_npad
         n2 += mosaic_npad
-        m1 = (1+size(z,1)) / n1 # add one because of mosaicview non-edge
-        m2 = (1+size(z,2)) / n2
-        plot_box! = (ox, oy) -> plot!(p,
-            ox .+ [0,1,1,0,0] * n1 .+ 0.0,
-            oy .+ [0,0,1,1,0] * n2 .+ 0.0,
-            line = jim_def[:line3type], label="")
-        for ii=0:n3-1
+        m1 = (1+size(zz,1)) ÷ n1 # add one because of mosaicview non-edge
+        m2 = (1+size(zz,2)) ÷ n2
+        fx = o -> x[1] + o * (x[2] - x[1])
+        fy = o -> y[1] + o * (y[2] - y[1])
+        if zyflip
+            fy = o -> y[end] - o * (y[2] - y[1])
+        end
+        function plot_box!(ox, oy)
+            plot!(p,
+                fx.(ox .+ [0,1,1,0,0] * n1 .- 1),
+                fy.(oy .+ [0,0,1,1,0] * n2 .- 1),
+                line = jim_def[:line3type], label="",
+            )
+        end
+        for ii=0:n3-1 # boxes around data only
             i1 = mod(ii, m1)
             i2 = ii ÷ m1
             plot_box!(i1*n1, i2*n2)
         end
     end
+
     plot!()
     gui && Plots.gui()
     prompt && MIRTjim.prompt()
--- test/jim.jl
+++ test/jim.jl
@@ -18,7 +18,6 @@ include("isplot.jl")
 @test_throws String jim(:bad)
 
 @isplot jim(ones(4,3), title="test2", xlabel=L"x")
-@isplot jim(rand(4,3,5), yflip=false, title=L"test3 x^2_i")
 @isplot jim(1:4, 5:9, zeros(4,5), title="test3", ylabel=L"y")
 @isplot jim(1:4, 5:9, zeros(4,5), "test3")
 @isplot jim(zeros(4,5), x=1:4, y=5:9, title="test3")
@@ -26,6 +25,13 @@ include("isplot.jl")
 @isplot jim(x=1:4, y=5:9, rand(4,5), title="test4")
 @isplot jim(x=-9:9, y=9:-1:-9, (-9:9) * (abs.((9:-1:-9) .- 5) .< 3)', title="rev")
 @isplot jim(ones(3,3)) # uniform
+
+#3D
+@isplot jim(rand(4,3,5), yflip=false, title=L"test3 x^2_i")
+@isplot jim(rand(4,3,5), ncol=2)
+@isplot jim(rand(4,3,5), nrow=3)
+@isplot jim(-3:2, -2:1, rand(6,4,4)) # zyflip
+
 jim(:abswarn, false)
 @isplot jim(rand(ComplexF32, 4,3))
 @isplot jim(rand(ComplexF32, 4,3), "complex 2d")
