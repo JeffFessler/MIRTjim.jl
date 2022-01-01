52ade0f5f75bfb9e12a80e2f586cd4844298b4fe
Jeff Fessler <fessler@umich.edu>
2022-01-01 10:38:42 -0500

    test

--- test/array.jl
+++ test/array.jl
@@ -4,3 +4,7 @@ z = [ones(6,4), rand(6,4), rand(6,4), rand(6,4), rand(6,4)]
 @isplot jim(z, ncol=2)
 @isplot jim(z, nrow=3)
 @isplot jim(-3:2, -2:1, z) # zyflip
+
+z = [reshape(1:12,3,4), ones(4,5)]
+@test MIRTjim._maxgood(z) == 12
+@test MIRTjim._mingood(z) == 1
