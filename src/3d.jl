774bdd89e00d83d60ba0dd2b792eff4a9e44e281
Jeff Fessler <fessler@umich.edu>
2022-06-26 22:40:13 -0400

    jim(x,y,z, array3d)

--- src/3d.jl
+++ src/3d.jl
@@ -4,8 +4,25 @@ export jim
 
 
 # 3D (or higher), built on array version
-function jim(z::AbstractArray{T} ; kwargs...) where {T <: Number}
+function jim(z::AbstractArray{<:Number} ; kwargs...)
     zz = reshape(z, size(z,1), size(z,2), :)
     out = [@view zz[:,:,i] for i in 1:size(zz,3)]
-    jim(out ; kwargs...)
+    return jim(out ; kwargs...)
+end
+
+
+"""
+    jim(x, y, z, array3d ; kwargs...)
+Allow user to provide
+the "z axis" of a 3D array,
+but ignore it without warning.
+"""
+function jim(
+    x::AbstractVector{<:RealU},
+    y::AbstractVector{<:RealU},
+    z::AbstractVector{<:RealU}, # ignored!
+    f::AbstractArray ; # could be 3D array or Vector of 2D arrays
+    kwargs...,
+)
+    return jim(f ; x, y, kwargs...)
 end
