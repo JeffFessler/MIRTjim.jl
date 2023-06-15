6af8b3da1fe4768c7c058ff9c9cb327116726188
Jeff Fessler <JeffFessler@users.noreply.github.com>
2023-06-14 22:31:37 -0400

    Support RGB

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,17 +10,37 @@ macro isplot(ex) # @isplot macro to streamline tests
 end
 
 @testset "MIRTjim" begin
+@show 88 # todo
     include("jim.jl")
 
     @test isempty(detect_ambiguities(MIRTjim))
 end
+throw() # todo
 
+list = [
+"axis.jl"
+"caller_name.jl"
+"color.jl"
+"offset.jl"
+"prompt.jl"
+"unit.jl"
+"mid3.jl"
+]
+
+for file in list[1:1]
+@show file # todo
+    include(file)
+end
+
+#= todo
 include("axis.jl")
 include("caller_name.jl")
+include("color.jl")
 include("offset.jl")
 include("prompt.jl")
 include("unit.jl")
 include("mid3.jl")
+=#
 
 @testset "array" begin
     include("array.jl")
